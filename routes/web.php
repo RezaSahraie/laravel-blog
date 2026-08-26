cbf72f7c987265ed922ed1761c11d2ba7774535d
Reza Sahraie <Sahraireza0@gmail.com>
2026-08-27 00:04:46 +0330

    feat(routes): add public post routes
    - Add posts.index and posts.show routes
    - Use slug-based route model binding for posts

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::inertia('/', 'welcome')->name('home');
+
+// Public blog routes
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{post:slug}', [PostController::class, 'show'])->name('posts.show');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
